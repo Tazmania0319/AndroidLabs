147e40342c0b9751aea9b7d63de9fe46dccf6f3d
Tazwar <afta0003@algonquinlive.com>
2022-09-27 13:16:14 -0400

    Wrong Orientation

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.Image;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView firstText = findViewById(R.id.inputtext);
+
+        EditText theEdit = findViewById(R.id.editText);
+
+        final Button btn = findViewById(R.id.button);
+        btn.setText("Press Me");
+        btn.setOnClickListener( (click) ->  { btn.setText("Label has been updated"); });
+
+        CheckBox cb = findViewById(R.id.checkBox);
+        cb.setOnCheckedChangeListener( (compoundButton, b) -> {
+
+            Snackbar.make(theEdit, "Checkbox is now " + b, Snackbar.LENGTH_LONG)
+                    .setAction("Undo", click-> compoundButton.setChecked( !b )).show();
+
+        });
     }
 }
